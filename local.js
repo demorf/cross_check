9be805174a986dd9dac43e36753bc5e61d91d50d
Dimitar Raev <dimitr.raev@gmail.com>
2020-04-30 21:01:01 +0300

    Dark Theme, fixed user link

---
+++ local.js
@@ -0,0 +1,4 @@
+function store_data(match){
+    path = match['my_algo'] + "\\" + match['my_algo'], match['enemy_algo_id'], match['match_id'], match['result']
+    writeFile( Path, Data, Callback)
+}
